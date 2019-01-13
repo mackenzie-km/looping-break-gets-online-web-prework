c28a1288ae8651eb40d382d75012aae51ac438fd
Mackenzie Moore <macmoore@ucdavis.edu>
2019-01-13 05:34:34 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,6 +3,6 @@ def levitation_quiz
     puts "What is the spell that enacts levitation?"
     answer = gets.chomp
     break if answer == "Wingardium Leviosa"
+  end
   puts "You passed the quiz!"
-end
 end
